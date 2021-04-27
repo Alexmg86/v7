7218935e10a5db668e3d85b54306991ae1f3d1ae
Alex <alexmg86@mail.ru>
2021-04-27 07:47:48 +0300

    init api

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+
+const app = createApp({})
+app.component('index', require('./components/Index.vue').default)
+app.mount('#app')
